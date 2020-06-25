b161eb31b1c150285b9e65fcd592ec28ba341cfa
Guilherme Canalli da Silva <canalg@alcoa.com>
2020-06-25 15:37:05 -0300

    Completed Tutorial Part 5

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -7,21 +8,33 @@ using System.Threading.Tasks;
 
 namespace ContosoUniversity.Models
 {
-    public class StudentVM
+    public class Instructor
     {
         public int ID { get; set; }
+
         [Required]
-        [StringLength(50)]
         [Display(Name = "Last Name")]
+        [StringLength(50)]
         public string LastName { get; set; }
+
         [Required]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Column("FirstName")]
         [Display(Name = "First Name")]
+        [StringLength(50)]
         public string FirstMidName { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Enrollment Date")]
-        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public OfficeAssignment OfficeAssignment {get;set;}
     }
 }
